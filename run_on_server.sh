4b84e5b7bfa6a16ce1250c511f5a8dcf2aeb4188
Jovan Rosario <Jovan_Rosario@Comcast.com>
2023-07-18 22:12:49 -0600

    Quality of Life Scripts

---
+++ run_on_server.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Should be run as on the server
+
+# Compile
+cd /home/ubuntu/nginx-1.18.0
+./configure --with-ld-opt="-lstdc++" --with-compat --add-dynamic-module=../diffstub --with-cc-opt=-Wno-error
+make
+sudo make install
+
+# Move to modules directory
+sudo mv /usr/local/nginx/modules/ngx_http_diffstub_module.so /usr/share/nginx/modules/ngx_http_diffstub_module.so
+
+sudo service nginx restart
