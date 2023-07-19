17af4d5954d2e7b3275c9315eecfd614e715e198
Jovan Rosario <Jovan_Rosario@Comcast.com>
2023-07-18 22:32:12 -0600

    Quality of Life Scripts

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Started from Alex Balk's (abalk200_comcast) diffstub project
 This is a standalone module for generateting an RF5261 (https://datatracker.ietf.org/doc/html/rfc5261) compliant MPED-DASH patch from 2 MPD files.
 
 https://github.com/MPEGGroup/DASHSchema/blob/5th-Ed-AMD1/DASH-MPD.xsd
+
+
+## Shell Scripts
+- First run "take_backup.sh" within VM (placed in the home directory). 
+- Then run "ship_to_server.sh" in local. 
+- Lastly, run "run_on_server" in the VM (placed in diffstub).
--- run_on_server.sh
+++ run_on_server.sh
@@ -2,12 +2,13 @@
 # Should be run as on the server
 
 # Compile
-cd /home/ubuntu/nginx-1.18.0
-./configure --with-ld-opt="-lstdc++" --with-compat --add-dynamic-module=../diffstub --with-cc-opt=-Wno-error
-make
-sudo make install
+cd /home/ubuntu/nginx-1.18.0 &&\
+./configure --with-ld-opt="-lstdc++" --with-compat --add-dynamic-module=../diffstub --with-cc-opt=-Wno-error &&\
+make &&\
+sudo make install &&\
 
 # Move to modules directory
-sudo mv /usr/local/nginx/modules/ngx_http_diffstub_module.so /usr/share/nginx/modules/ngx_http_diffstub_module.so
+sudo mv /usr/local/nginx/modules/ngx_http_diffstub_module.so /usr/share/nginx/modules/ngx_http_diffstub_module.so &&\
 
-sudo service nginx restart
+sudo service nginx restart &&\
+echo "Successfully completed run_on_server"
