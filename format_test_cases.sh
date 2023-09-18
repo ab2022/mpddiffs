74e24a03c4b9ca9dc00a85fe3220554e8e48853d
Erik Ponder <Erik_Ponder@comcast.com>
2023-09-18 12:05:29 -0600

    Formatted test cases, add MPD formatting script

---
+++ format_test_cases.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Set the directory containing your XML files
+test_dir="/Users/eponde494/Workspace/diffstub/mpd_samples/test_cases"
+
+# Loop through all XML files in the directory
+for file in "$test_dir"/*.mpd; do
+    if [ -f "$file" ]; then
+        # Format the XML file with 4-space tab indents and save it with the original name
+        xmllint --format --encode UTF-8 "$file" > "$file.tmp" && mv "$file.tmp" "$file"
+    fi
+done
