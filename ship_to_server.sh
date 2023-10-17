e119c564cf41cb211a839968bd4b20800ec06fea
Erik Ponder <Erik_Ponder@comcast.com>
2023-10-17 12:00:19 -0600

    Updated scripts to be more modular

--- ship_to_server.sh
+++ ship_to_server.sh
@@ -3,28 +3,29 @@
 source_path="../diffstub"
 destination_user="ubuntu"
 destination_host="35.88.227.80"
+destination_port="20202"
 destination_path="/home/ubuntu/diffstub"
 
 # Exclude git-related files using the --exclude flag with tar
-tar --exclude=".git" --exclude=".gitignore" -czf /tmp/archive.tar.gz -C $source_path .
+tar --exclude=".git" --exclude=".gitignore" --exclude="mpd_samples" -czf /tmp/archive.tar.gz -C $source_path .
 echo 'tar created'
 
 # Take Diffstub backup
-ssh -p 20202 -i ~/.ssh/id_rsa_aws $destination_user@$destination_host "mv /home/ubuntu/diffstub /home/ubuntu/diffstub_$(date +%s)_backup"
+ssh -p $destination_port $destination_user@$destination_host "mv /home/ubuntu/diffstub /home/ubuntu/diffstub_$(date +%s)_backup"
 echo 'diffstub backed up'
 
 # Create new empty diffstub directory
-ssh -p 20202 -i ~/.ssh/id_rsa_aws $destination_user@$destination_host "mkdir /home/ubuntu/diffstub"
+ssh -p $destination_port $destination_user@$destination_host "mkdir /home/ubuntu/diffstub"
 echo 'diffstub created'
 
 # Transfer the archived files to the VM using scp
-scp -P 20202 -i ~/.ssh/id_rsa_aws /tmp/archive.tar.gz $destination_user@$destination_host:$destination_path
+scp -P $destination_port /tmp/archive.tar.gz $destination_user@$destination_host:$destination_path
 echo 'tar transferred'
 
 # Extract the files on the VM using tar
-ssh -p 20202 -i ~/.ssh/id_rsa_aws $destination_user@$destination_host "tar -xzf $destination_path/archive.tar.gz -C $destination_path"
+ssh -p $destination_port $destination_user@$destination_host "tar -xzf $destination_path/archive.tar.gz -C $destination_path"
 echo 'tar extracted'
 
 # Remove the temporary archive file on the VM
-ssh -p 20202 -i ~/.ssh/id_rsa_aws $destination_user@$destination_host "rm $destination_path/archive.tar.gz"
+ssh -p $destination_port $destination_user@$destination_host "rm $destination_path/archive.tar.gz"
 echo 'tar removed'
